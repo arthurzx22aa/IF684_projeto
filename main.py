821eb4cca2dd98cff29badcb6d21cc5544a970ed
Wellinne <wellinnenascimento@gmail.com>
2024-06-05 11:37:09 -0300

    função do uniformCost sem alteração na main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import sys
 import pygame
 from config import BLOCK_SIZE, WHITE
 from elements import draw_player
+from node import path_manhattan
 from world import draw_highlight_overlay, draw_map, draw_overlay, draw_path, generate_map
 from tile import TILE_WEIGHTS, FOOD_TILE
-from uniformCost import uniform_cost_search  # Importando a função
 
 # initialize pygame
 pygame.init()
@@ -48,16 +48,16 @@ while running:
             if event.key == pygame.K_LEFT:
                 agent_pos[0] = max(agent_pos[0] - 1, 0)
             elif event.key == pygame.K_RIGHT:
-                agent_pos[0] = min(agent_pos[0] + 1, SCREEN_WIDTH // BLOCK_SIZE - 1)
+                agent_pos[0] = min(agent_pos[0] + 1, SCREEN_WIDTH - 1)
             elif event.key == pygame.K_UP:
                 agent_pos[1] = max(agent_pos[1] - 1, 0)
             elif event.key == pygame.K_DOWN:
-                agent_pos[1] = min(agent_pos[1] + 1, SCREEN_HEIGHT // BLOCK_SIZE - 1)
+                agent_pos[1] = min(agent_pos[1] + 1, SCREEN_HEIGHT - 1)
             elif event.key == pygame.K_F1:
                 overlay = not overlay
             elif event.key == pygame.K_F2:
                 highlight_overlay = not highlight_overlay
-        elif event.type == pygame.MOUSEBUTTONDOWN:  # add food on mouse click
+        elif event.type == pygame.MOUSEBUTTONDOWN: # add food on mouse click
             if event.button == 1:  # left mouse button
                 # get the position of the mouse click
                 x, y = event.pos
@@ -67,17 +67,17 @@ while running:
                 game_map[food_position[1]][food_position[0]] = FOOD_TILE
 
                 # call pathfinding algorithm
-                path, _ = uniform_cost_search(game_map, tuple(agent_pos), food_position)
-
+                path = path_manhattan(agent_pos, food_position)
+                
                 # reset path variables
-                current_step = 0
+                current_step = 0 
                 next_move_time = current_time + move_delay
 
     # path update logic
-    if path:
+    if path is not None:
         if food_position and current_step < len(path):
             if current_time >= next_move_time:
-                agent_pos = list(path[current_step])
+                agent_pos = path[current_step]
                 current_step += 1
                 # update next move time
                 # based on the cost of the current tile
